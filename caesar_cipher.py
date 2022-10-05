3101d9579b2edc2efb41b1ae0f5ea3265be2e264
Ondairos <ondairos@gmail.com>
2022-10-05 11:35:12 +0300

    Restructured the functions into one function that solves the problem

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -7,28 +7,15 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-  cipher_text = "" #create empty string
-  for letter in plain_text: #pass through all letters in first argument
-    position = alphabet.index(letter) #find the index of the letter
-    new_position = position + shift_amount #calculate the new position
-    new_letter = alphabet[new_position] #create the new encrypted letter
-    cipher_text += new_letter #create the encrypted word
-    cipher_text_solved = cipher_text
-  print(f"The encoded text is {cipher_text}")
-
-
-def decrypt(cipher_text, shift_amount):
-  decrypt_text =""
-  for letter in cipher_text:
+def caesar(text, shift, direction):
+  plain_text = ""
+  for letter in text:
     position = alphabet.index(letter)
-    new_position = position - shift_amount
-    new_letter = alphabet[new_position]
-    decrypt_text += alphabet[new_position]
-  print(f"Your decrypted word is: {decrypt_text}")
+    if direction == "encode":
+      new_position = position + shift
+    elif direction == "decode":
+      new_position = position - shift
+    plain_text += alphabet[new_position]  
+  print(f"Your word is : {plain_text}")
 
-#encrypt function and pass in the user inputs.
-if direction == "encode":
-  encrypt(plain_text=text, shift_amount=shift)
-elif direction =="decode":
-  decrypt(cipher_text=text,shift_amount=shift)
+caesar(text=text, shift=shift, direction=direction)
